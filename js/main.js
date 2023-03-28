ba6b282bfe982c2e084c3f15e0fbda349acfa166
Miss_terry <Etein@yandex.ru>
2023-03-28 16:16:06 +0300

    добавлен адрес для отправки формы

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 import './minis-generator.js';
 import { onMiniPicClick } from './big-picture.js';
 import { picturesContainer } from './big-picture.js';
-
 import {imgInput, onUploadImg, imgUploadForm, onSubmit} from './upload-image.js';
-
 
 picturesContainer.addEventListener('click', onMiniPicClick);
 imgInput.addEventListener('change', onUploadImg);
