84e130908e026439b284ce7b691f78e45ca71098
Keks <keks@htmlacademy.ru>
2023-04-09 19:41:31 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/util.js
@@ -0,0 +1,58 @@
+const ALERT_SHOW_TIME = 5000;
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+function createUniqueId (min, max) {
+  const idArray = [];
+
+  return function () {
+    let singleId = getRandomInteger(min, max);
+    if (idArray.length >= (max - min + 1)) {
+      return null;
+    }
+    while(idArray.includes(singleId)) {
+      singleId = getRandomInteger(min, max);
+    }
+    idArray.push(singleId);
+    return singleId;
+  };
+}
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export { createUniqueId, getRandomInteger, getRandomArrayElement, isEscapeKey, showAlert, debounce };
