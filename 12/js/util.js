a14c1df92b2b1cff3ef51341f6f3d102b1279e04
Keks <keks@htmlacademy.ru>
2023-04-12 15:24:05 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/util.js
+++ 12/js/util.js
@@ -1,3 +1,4 @@
+const ALERT_SHOW_TIME = 5000;
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const getRandomInteger = (min, max) => {
@@ -25,6 +26,27 @@ function createUniqueId (min, max) {
   };
 }
 
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
 const debounce = (callback, timeoutDelay) => {
   let timeoutId;
   return (...rest) => {
@@ -33,4 +55,4 @@ const debounce = (callback, timeoutDelay) => {
   };
 };
 
-export { createUniqueId, getRandomInteger, getRandomArrayElement, isEscapeKey, debounce };
+export { createUniqueId, getRandomInteger, getRandomArrayElement, isEscapeKey, showAlert, debounce };
